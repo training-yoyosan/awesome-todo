7f9fb7408560f1ce03950f2a9e9baa4a2c9d23c7
Yoyo San <veselu@gmail.com>
2020-03-16 14:30:18 +0200

    Add component exercise.

---
+++ src/components/Task.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <div>
+      {{ task.name }} {{ index }}
+      <button @click="deleteTask(index)">X</button>
+    </div>
+
+    <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['task', 'index']
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Components.vue
+++ src/pages/Components.vue
@@ -1,10 +1,49 @@
 <template>
   <q-page padding>
+    <ul>
+      <task
+        v-for="task in tasks" :key="task.id"
+        :task="task"
+        :index="task.id"
+      ></task>
+    </ul>
   </q-page>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'Go to dentist',
+          dueDate: '2019/05/12',
+          dueTime: '18:30'
+        },
+        {
+          id: 2,
+          name: 'Go shopping',
+          dueDate: '2019/05/13',
+          dueTime: '14:00'
+        },
+        {
+          id: 3,
+          name: 'Go to the cinema',
+          dueDate: '2019/05/14',
+          dueTime: '14:30'
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTask (index) {
+      this.tasks.splice(index, 1)
+    }
+  },
+  components: {
+    task: require('components/Task.vue').default
+  }
 }
 </script>
 
