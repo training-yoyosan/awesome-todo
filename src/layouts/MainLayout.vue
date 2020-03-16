aeeacdfbf03976449f5009e2499b75231731513e
Yoyo San <veselu@gmail.com>
2020-03-16 13:57:33 +0200

    Fix links in burger menu.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -61,40 +61,16 @@ export default {
       leftDrawerOpen: false,
       essentialLinks: [
         {
-          title: 'Docs',
-          caption: 'quasar.dev',
+          title: 'Section 3',
+          caption: 'Section 3 of the course.',
           icon: 'school',
-          link: 'https://quasar.dev'
+          link: '/'
         },
         {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
+          title: 'Section 4',
+          caption: 'Section 4 of the course.',
           icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
+          link: '/components'
         }
       ]
     }
